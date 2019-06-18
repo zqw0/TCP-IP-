0ce571515c0e0e0d106333515ba901ee3a7c7f56
0 <51182128+zqw0@users.noreply.github.com>
2019-06-18 16:49:44 +0800

    Create 回声服务端.c

---
+++ 回声服务端.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<arpa/inet.h>
+#include<sys/socket.h>
+#define BUF_SIZE 1024
+void error_handling(char *message);
+int main(int argc,char *argv[])
+{
+    int serv_sock,clnt_sock,len;
+
+    struct sockaddr_in serv_addr,clnt_addr;
+    socklen_t clnt_addr_size,i;
+
+    char message[]="hello world";
+
+    if(argc!=2){
+        printf("Usage:%s <port>\n",argv[0]);
+        exit(1);
+    }
+
+    serv_sock=socket(PF_INET,SOCK_STREAM,0);
+    if(serv_sock==-1) error_handling("socket error");
+
+    memset(&serv_addr,0,sizeof(serv_addr));
+    serv_addr.sin_family=AF_INET;
+    serv_addr.sin_addr.s_addr=htonl(INADDR_ANY);///htonl将小端序转换为大端序
+    serv_addr.sin_port=htons(atoi(argv[1]));    ///htons将小端序转换为大端序
+
+    if(bind(serv_sock,(struct *sockaddr)&serv_addr,sizeof(serv_addr))==-1)
+        error_handling("bind error");
+
+    if(listen(serv_sock,5)==-1)
+        error_handling("listen error");
+
+    clnt_addr_size=sizeof(clnt_addr);
+    for(int i=0;i<5;i++){
+        clnt_sock=accept(serv_sock,(struct *sockaddr)&clnt_addr,&clnt_addr_size);
+        if(clnt_sock==-1) error_handling("accept error");
+        else printf("Connect client %d\n",i+1);
+        
+        while((len=read(clnt_sock,message,BUF_SIZE))!=0)
+            write(clnt_sock,message,len);
+        
+        close(clnt_sock);
+    }
+    close(serv_sock);
+    return 0;
+}
+
+void error_handling(char *message){
+    fputs(message,stderr);
+    fputs('\n',stderr);
+    exit(1);
+}
